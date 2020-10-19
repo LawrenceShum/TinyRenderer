302682b165b2cf012d49f8a6c635b62333be368e
LawrenceShum <lawrencesom1104@gmal.com>
2020-10-19 14:00:05 +0800

    udpate

--- TinyRenderer/main.cpp
+++ TinyRenderer/main.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+﻿#include <iostream>
 #include "tgaimage.h"
 #include <cmath>
+
+using namespace std;
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
@@ -8,10 +10,33 @@ const TGAColor green = TGAColor(0, 128, 0, 255);
 const TGAColor yellow = TGAColor(255, 255, 0, 255);
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-	for (float t = 0.; t < 1.; t += .001) {
-		int x = x0 + (x1 - x0)*t;
-		int y = y0 + (y1 - y0)*t;
-		image.set(x, y, color);
+	bool steep = false;
+	if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
+		std::swap(x0, y0);
+		std::swap(x1, y1);
+		steep = true;
+	}
+	if (x0 > x1) {
+		std::swap(x0, x1);
+		std::swap(y0, y1);
+	}
+	int dx = x1 - x0;
+	int dy = y1 - y0;
+	int derror2 = std::abs(dy) * 2;
+	int error2 = 0;
+	int y = y0;
+	for (int x = x0; x <= x1; x++) {
+		if (steep) {
+			image.set(y, x, color);
+		}
+		else {
+			image.set(x, y, color);
+		}
+		error2 += derror2;
+		if (error2 > dx) {
+			y += (y1 > y0 ? 1 : -1);
+			error2 -= dx * 2;
+		}
 	}
 }
 
@@ -39,8 +64,8 @@ void circle(int orient_x, int orient_y, int radius, TGAImage &image, TGAColor in
 
 int main(int argc, char** argv) {
 	TGAImage image(500, 500, TGAImage::RGB);
-	circle(250, 250, 100, image, white);
-	//line(13, 20, 80, 40, image, red);
+	//circle(250, 250, 100, image, white);
+	line(400, 350, 80, 40, image, white);
 	image.write_tga_file("output.tga"); 
 	return 0;
 }
